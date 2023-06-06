52a9b7b20fc57610ff5fc86ee3f814c99642484e
Igor Muller <igormullersantos@hotmail.com>
2023-06-06 11:53:55 -0300

    Fetch character e envia props

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,47 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from "vue-router";
+export default {
+  data() {
+    return {
+      characters: {},
+      prevCharacters: null,
+      nextCharacters: null,
+    };
+  },
+  methods: {
+    getCharacters(url) {
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          this.prevCharacters = data.info.prev;
+          this.nextCharacters = data.info.next;
+          this.characters = data.results;
+        });
+    },
+    prev() {
+      this.getCharacters(this.prevCharacters);
+    },
+    next() {
+      this.getCharacters(this.nextCharacters);
+    },
+  },
+  mounted() {
+    this.getCharacters("https://rickandmortyapi.com/api/character");
+  },
+};
 </script>
 
 <template>
-  <RouterView />
+  <header>
+    <RouterLink to="/"
+      ><img
+        class="logo"
+        src="../src/assets/images/rick-and-morty.png"
+        alt="Logo"
+    /></RouterLink>
+  </header>
+
+  <RouterView :characters="characters" @prev="prev" @next="next" />
 </template>
 
 <style scoped></style>
