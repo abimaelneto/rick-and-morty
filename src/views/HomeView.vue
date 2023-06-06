c69dbf69f7e517d4f9757c16abee001ff999eba6
Igor Muller <igormullersantos@hotmail.com>
2023-06-06 11:54:09 -0300

    recebe props e manipula

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,50 @@
-<script setup></script>
+<script>
+export default {
+  props: {
+    characters: Object,
+  },
+  data() {
+    return {
+      characterId: this.$props.characters.id,
+    };
+  },
+  methods: {
+    prev() {
+      this.$emit("prev");
+    },
+    next() {
+      this.$emit("next");
+    },
+  },
+};
+</script>
 
 <template>
-  <main></main>
+  <main>
+    <div class="characters" v-for="character in characters">
+      <img :src="`${character.image}`" alt="" />
+      <h2>{{ character.name }}</h2>
+
+      <h3>{{ character.species }}</h3>
+
+      <h3>{{ character.status }}</h3>
+
+      <p>Last Seen</p>
+      <h3>{{ character.location.name }}</h3>
+    </div>
+  </main>
+  <div class="moreCharacters">
+    <button @click="prev">
+      <span class="material-symbols-rounded more prev"> chevron_left </span>
+    </button>
+    <button @click="next">
+      <span class="material-symbols-rounded more next"> chevron_right </span>
+    </button>
+  </div>
 </template>
+
+<style scoped>
+main {
+  display: flex;
+}
+</style>
