ecc588eec999a0e0d9c0d91cea8e772da78de2d9
Igor Muller <igormullersantos@hotmail.com>
2023-06-07 16:14:34 -0300

    Estilo residents

--- src/components/Residents.vue
+++ src/components/Residents.vue
@@ -6,10 +6,33 @@ export default {
 };
 </script>
 <template>
-  <div class="residents" v-for="resident in residents" :key="resident.id">
-    <img :src="`${resident.image}`" :alt="`${resident.name}`" />
-    <RouterLink :to="`/character/${resident.id}`">
-      <h3>{{ resident.name }}</h3>
-    </RouterLink>
+  <div class="container">
+    <div class="residents" v-for="resident in residents" :key="resident.id">
+      <RouterLink :to="`/character/${resident.id}`">
+        <img :src="`${resident.image}`" :alt="`${resident.name}`" />
+        <h3>{{ resident.name }}</h3>
+      </RouterLink>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.residents {
+  margin: 2rem;
+}
+
+img {
+  border-radius: 50%;
+}
+
+h3 {
+  margin-top: 1rem;
+  font-size: 1.5rem;
+  color: white;
+  transition: all ease 0.3s;
+}
+
+h3:hover {
+  color: #00ff55;
+}
+</style>
