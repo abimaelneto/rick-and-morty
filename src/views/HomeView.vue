5a4a19c8ba9855591a13504d64b4550ebf7b980d
Igor Muller <igormullersantos@hotmail.com>
2023-06-07 12:08:41 -0300

    Adicionando função de search

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,8 @@
 export default {
   data() {
     return {
-      characters: {},
+      characters: [],
+      search: "",
       prevCharacters: null,
       nextCharacters: null,
     };
@@ -23,6 +24,17 @@ export default {
     next() {
       this.getCharacters(this.nextCharacters);
     },
+    filtered() {
+      if (!this.search) return this.characters;
+
+      const filter = [];
+      for (const character of this.characters) {
+        if (character.name.toLowerCase().includes(this.search.toLowerCase())) {
+          filter.push(character);
+        }
+      }
+      return filter;
+    },
   },
   mounted() {
     this.getCharacters("https://rickandmortyapi.com/api/character");
@@ -32,7 +44,9 @@ export default {
 
 <template>
   <main>
-    <div class="characters" v-for="character in characters">
+    <input v-model="search" placeholder="Search Character..." />
+
+    <div class="character" v-for="character in filtered()" :key="character.id">
       <img :src="`${character.image}`" alt="" />
       <h2>{{ character.name }}</h2>
 
@@ -47,19 +61,19 @@ export default {
         <h3>{{ character.location.name }}</h3>
       </RouterLink>
     </div>
+    <div class="item error" v-if="this.search && !filtered().length">
+      <p>No results found!</p>
+    </div>
+
+    <div class="moreCharacters">
+      <button @click="prev">
+        <span class="material-symbols-rounded more prev"> chevron_left </span>
+      </button>
+      <button @click="next">
+        <span class="material-symbols-rounded more next"> chevron_right </span>
+      </button>
+    </div>
   </main>
-  <div class="moreCharacters">
-    <button @click="prev">
-      <span class="material-symbols-rounded more prev"> chevron_left </span>
-    </button>
-    <button @click="next">
-      <span class="material-symbols-rounded more next"> chevron_right </span>
-    </button>
-  </div>
 </template>
 
-<style scoped>
-main {
-  display: flex;
-}
-</style>
+<style scoped></style>
