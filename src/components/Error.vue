fcfc7b6b37f47b016b79bd5ce7ce7ff7107ab0b8
Igor Muller <igormullersantos@hotmail.com>
2023-06-12 11:19:38 -0300

    Criando componente de erro

---
+++ src/components/Error.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+<template>
+  <div class="error center">
+    <h2 class="title">CHARACTER NOT FOUND</h2>
+    <iframe src="https://embed.lottiefiles.com/animation/39138"></iframe>
+
+    <button @click="close">
+      <span class="material-symbols-rounded btn"> close </span>
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.error {
+  position: fixed;
+  height: 100vh;
+  width: 100vw;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  flex-direction: column;
+  background-color: rgba(0, 0, 0, 0.562);
+  color: #00ff55;
+  text-align: center;
+}
+h2 {
+  background: transparent;
+}
+
+button {
+  border: none;
+  background: none;
+  margin-top: 1rem;
+}
+</style>
